17769433e55a5b9dd27e2f1bd3a80bc3c21f2a1a
Adamasscode <39639630+Adamasscode@users.noreply.github.com>
2018-10-24 20:47:52 +1100

    PCA

--- read.py
+++ read.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import numpy as np
+from sklearn.decomposition import PCA
 
 def Sparse_form(X, y, len_y, D):
     sparseMat = np.zeros((len_y, D))
@@ -31,8 +32,8 @@ def Load_Data(path='./conll_train'):
     return data_x, data_y
 
 def Select_Data_Mat(data_x, data_y, index, D):
-    print(data_x[index - 1])
-    print(data_y[index - 1])
+    # print(data_x[index - 1])
+    # print(data_y[index - 1])
     sparseMat = Sparse_form(data_x[index - 1], data_y[index - 1], len(data_y[index - 1]), D)
     return sparseMat
 
@@ -49,18 +50,67 @@ def Merge_Data_Mat(data_x, data_y, rate, D):
         print(index)
     return mergeMat
 
+def PCA_data(x):
+    pca = PCA(n_components=100)
+    print(x.shape)
+    pca.fit(x)
+    data_x_pca = pca.transform(x)
+    return data_x_pca
+
+def PCA_All_Data_Mat(data_x,data_y,D):
+    pca_mergeMat = np.array([0,0,0])
+    while len(data_x) // 8 > 1:
+        data_x_temp = data_x[:8]
+        data_x = data_x[8:]
+        data_y_temp = data_y[:8]
+        data_y = data_y[8:]
+        mergeMat = Sparse_form(data_x_temp[0], data_y_temp[0], len(data_y_temp[0]), D)
+        for i in range(1,len(data_y_temp)):
+            newMat = Sparse_form(data_x_temp[i], data_y_temp[i], len(data_y_temp[i]), D)
+            print(i)
+            mergeMat = np.concatenate((mergeMat, newMat), axis = 0)
+            print('new_long')
+        if pca_mergeMat.any() == 0:
+            print('pca0_long')
+            pca_mergeMat = PCA_data(mergeMat)
+        else:
+            print('pca1_long')
+            pca_newMat = PCA_data(mergeMat)
+            print('pca2_long')
+            pca_mergeMat = np.concatenate((pca_mergeMat, pca_newMat), axis = 0)
+    print(pca_mergeMat.shape)
+    mergeMat = Sparse_form(data_x[0], data_y[0], len(data_y[0]), D)
+    for i in range(1,len(data_y)):
+        newMat = Sparse_form(data_x[i], data_y[i], len(data_y[i]), D)
+        print(i)
+        mergeMat = np.concatenate((mergeMat, newMat), axis = 0)
+        print('new')
+    if pca_mergeMat.any() == 0:
+        print('pca0')
+        pca_mergeMat = PCA_data(mergeMat)
+    else:
+        print('pca1')
+        pca_newMat = PCA_data(mergeMat)
+        print('pca2')
+        pca_mergeMat = np.concatenate((pca_mergeMat, pca_newMat), axis = 0)
+    return pca_mergeMat
+
 
 data_x, data_y = Load_Data()
+
+D = 2035523
 # print(data_x[23 - 1])
 # print(data_y[23 - 1])
-D = 2035523
+data_x_pca = PCA_All_Data_Mat(data_x[:23],data_y[:23],D)
+print(data_x_pca.shape)
 # random_list = np.random.randint(1,8936,10)
 # print(random_list)
 # matrix = Select_Data_Mat(data_x,data_y,23,D)
-rate = 0.01
-merge_matrix = Merge_Data_Mat(data_x, data_y, rate, D)
+# rate = 0.01
+# merge_matrix = Merge_Data_Mat(data_x, data_y, rate, D)
 # print(matrix)
-print(merge_matrix)
+# print(merge_matrix)
+print(data_x_pca)
 print('finish')
 # for i in data:
 #     print(i)
