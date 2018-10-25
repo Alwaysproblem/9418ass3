042a099054ecec88c5105ad6ae59e11715471c49
Alwaysproblem <reganyang0415@gmail.com>
2018-10-25 21:56:55 +1100

    add softmax

--- CStrain.py
+++ CStrain.py
@@ -4,22 +4,15 @@ import numpy as np
 # from queue import Queue
 from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.gaussian_process.kernels import RBF
+from sklearn.metrics import accuracy_score
+import tensorflow as tf
 
 tarin_path = './conll_train'
 dev_path = './conll_dev'
 
 np.random.seed(1)
 
-D = 2035523
-# define the dimentional of give example sparse matrix.
-
-Comp_dims = 100
-#define the dimentional of compressed matrix.
-
-A = np.mat(np.random.randn(Comp_dims, D))
-# y = A * s, the s is sparse-matrix. the y is compressed measurements.
-
-def Load_Data(path='./conll_train', sample = None, replace = False):
+def Load_Data(A, Comp_dims, path='./conll_train', sample = None, replace = False):
     x_comp_data = []
     y_label = []
     name = path.split("_")[-1]
@@ -81,17 +74,66 @@ def Load_Data(path='./conll_train', sample = None, replace = False):
     return np.concatenate(x_comp_data, axis = 0), np.concatenate(y_label, axis = 0)
 
 
+def softmax_classfier(input_data, label, Comp_dims, class_num, X_test, y_test):
+    print(input_data.shape)
+    print(label.shape)
+    input_in = tf.placeholder(tf.float32, shape=(None, Comp_dims), name="input_data")
+    lab = tf.placeholder(tf.uint8, shape=(None,), name="label")
+    pred = tf.layers.dense(input_in, class_num, activation=tf.nn.softmax)
+    print(pred.get_shape())
+    y_label = tf.one_hot(lab, class_num)
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(
+                        logits = pred, 
+                        labels = y_label
+                    )
+    loss = tf.reduce_mean(cross_entropy, name = "loss")
+    opt = tf.train.AdamOptimizer().minimize(loss)
+    acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(pred, 1), tf.argmax(y_label, 1)), dtype = tf.float32), name = "accuracy")
+
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as s:
+        s.run(init)
+        _, train_acc = s.run([opt, acc], feed_dict={input_in: input_data, lab: label})
+        test_acc = s.run([acc], feed_dict={input_in: X_test, lab: y_test})
+
+    return train_acc, test_acc[0]
+
+
 def main():
-    X_train, y_train = Load_Data(sample=0.1)
-    kernel = 1.0 * RBF(Comp_dims)
-    gpc = GaussianProcessClassifier(kernel=kernel)
-    gpc.fit(X_train, y_train)
-    train_acc = gpc.score(X_train, y_train)
+
+    D = 2035523
+    # define the dimentional of give example sparse matrix.
+
+    Comp_dims = 50
+    #define the dimentional of compressed matrix.
+
+    A = np.mat(np.random.randn(Comp_dims, D))
+    # y = A * s, the s is sparse-matrix. the y is compressed measurements.
+
+    C = 23
+
+    X_train, y_train = Load_Data(A, Comp_dims, sample=0.05)
+    X_dev, y_dev = Load_Data(A, Comp_dims, path=dev_path, sample=0.05)
+
+    print("start training...")
+
+    train_acc, dev_acc = softmax_classfier(X_train, y_train, Comp_dims, C, X_dev, y_dev)
+
+    # kernel = 1.0 * RBF(Comp_dims)
+    # gpc = GaussianProcessClassifier(kernel=kernel)
+    # gpc.fit(X_train, y_train)
+    # y_train_pred = gpc.predict(X_train)
+
+    # train_acc = gpc.score(X_train, y_train)
+    # train_acc = accuracy_score(y_train, y_train_pred)
+
     print(f"the train accuracy is {train_acc}")
-    X_train, y_train = Load_Data()
-    X_dev, y_dev = Load_Data(path=dev_path, sample=0.1)
-    dev_acc = gpc.score(X_dev, y_dev)
-    print(f"the train accuracy is {dev_acc}")
+
+    # y_dev_pred = gpc.predict(X_dev)
+    # # dev_acc = gpc.score(X_dev, y_dev)
+    # dev_acc = accuracy_score(y_dev, y_dev_pred)
+    print(f"the cross validation accuracy is {dev_acc}")
 
 
 if __name__ == '__main__':
