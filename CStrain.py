05d732b9cec1bf154c9eef2d1188107c8af57239
Alwaysproblem <reganyang0415@gmail.com>
2018-10-26 09:41:57 +1100

    add softmax classifier

--- CStrain.py
+++ CStrain.py
@@ -75,29 +75,32 @@ def Load_Data(A, Comp_dims, path='./conll_train', sample = None, replace = False
 
 
 def softmax_classfier(input_data, label, Comp_dims, class_num, X_test, y_test):
-    print(input_data.shape)
-    print(label.shape)
+    # print(input_data.shape)
+    # print(label.shape)
     input_in = tf.placeholder(tf.float32, shape=(None, Comp_dims), name="input_data")
-    lab = tf.placeholder(tf.uint8, shape=(None,), name="label")
+    lab = tf.placeholder(tf.int32, shape=(None,), name="label")
+
     pred = tf.layers.dense(input_in, class_num, activation=tf.nn.softmax)
-    print(pred.get_shape())
+    # print(pred.get_shape())
     y_label = tf.one_hot(lab, class_num)
+
     cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(
                         logits = pred, 
                         labels = y_label
                     )
     loss = tf.reduce_mean(cross_entropy, name = "loss")
-    opt = tf.train.AdamOptimizer().minimize(loss)
+    opt = tf.train.AdamOptimizer(0.1).minimize(loss)
     acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(pred, 1), tf.argmax(y_label, 1)), dtype = tf.float32), name = "accuracy")
 
     init = tf.global_variables_initializer()
 
     with tf.Session() as s:
         s.run(init)
-        _, train_acc = s.run([opt, acc], feed_dict={input_in: input_data, lab: label})
-        test_acc = s.run([acc], feed_dict={input_in: X_test, lab: y_test})
+        s.run(opt, feed_dict={input_in: input_data, lab: label})
+        train_acc = s.run(acc, feed_dict={input_in: input_data, lab: label})
+        test_acc = s.run(acc, feed_dict={input_in: X_test, lab: y_test})
 
-    return train_acc, test_acc[0]
+    return train_acc, test_acc
 
 
 def main():
@@ -113,8 +116,8 @@ def main():
 
     C = 23
 
-    X_train, y_train = Load_Data(A, Comp_dims, sample=0.05)
-    X_dev, y_dev = Load_Data(A, Comp_dims, path=dev_path, sample=0.05)
+    X_train, y_train = Load_Data(A, Comp_dims, sample=0.005)
+    X_dev, y_dev = Load_Data(A, Comp_dims, path=dev_path, sample=0.005)
 
     print("start training...")
 
@@ -123,16 +126,15 @@ def main():
     # kernel = 1.0 * RBF(Comp_dims)
     # gpc = GaussianProcessClassifier(kernel=kernel)
     # gpc.fit(X_train, y_train)
-    # y_train_pred = gpc.predict(X_train)
 
-    # train_acc = gpc.score(X_train, y_train)
+    # y_train_pred = gpc.predict(X_train)
     # train_acc = accuracy_score(y_train, y_train_pred)
 
     print(f"the train accuracy is {train_acc}")
 
     # y_dev_pred = gpc.predict(X_dev)
-    # # dev_acc = gpc.score(X_dev, y_dev)
     # dev_acc = accuracy_score(y_dev, y_dev_pred)
+
     print(f"the cross validation accuracy is {dev_acc}")
 
 
